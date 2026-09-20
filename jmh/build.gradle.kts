74a1568e0c153f6d8c80ef1db7c4689ef2cbaed1
Mitsunori Komatsu <komamitsu@gmail.com>
2026-09-20 12:51:35 +0900

    Remove duplicated JMH profiler and include settings

--- jmh/build.gradle.kts
+++ jmh/build.gradle.kts
@@ -23,10 +23,6 @@ jmh {
     profilers = providers.gradleProperty("jmhProfilers").map { it.split(",") }.orElse(emptyList())
     // ./gradlew :jmh:jmh -PjmhIncludes=WriteUTF8String
     includes = providers.gradleProperty("jmhIncludes").map { it.split(",") }.orElse(emptyList())
-    // ./gradlew :jmh:jmh -PjmhProfilers=gc
-    profilers = providers.gradleProperty("jmhProfilers").map { it.split(",") }.orElse(emptyList())
-    // ./gradlew :jmh:jmh -PjmhIncludes=WriteUTF8String
-    includes = providers.gradleProperty("jmhIncludes").map { it.split(",") }.orElse(emptyList())
     jvmArgs = listOf(
         "--add-opens=java.base/java.nio=ALL-UNNAMED",
         "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED",
